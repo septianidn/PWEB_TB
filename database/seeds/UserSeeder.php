848e13133003ccc0d17d21104abd6dc4d486ea47
septianidn <eltianhost@gmail.com>
2021-06-01 22:08:02 +0700

    Menambahkan seeder untuk user admin

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::create([
+            'username'	=> 'admin',
+            'email'	=> 'admin@gmail.com',
+            'role' => '1',
+            'password'	=> bcrypt('secret')
+]);
+    }
+}
